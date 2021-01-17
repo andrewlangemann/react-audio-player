073729896fc6d0502b07fa2d9126f103765766de
Andrew Langemann <andrew.langemann@arcurve.com>
2021-01-17 11:48:19 -0700

    More work

--- src/components/CallRecordingPlayer.tsx
+++ src/components/CallRecordingPlayer.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 
-import { Button, Container, Grid, IconButton, makeStyles, MenuItem, Paper, Select, Slider, Typography } from "@material-ui/core";
+import { Grid, IconButton, makeStyles, MenuItem, Paper, Select, Slider, Typography } from "@material-ui/core";
 
 import {
-    FastForwardOutlined,
-    FastRewindOutlined,
     Forward10Outlined,
     PauseCircleOutlineOutlined,
     PlayCircleOutlineOutlined,
@@ -21,10 +19,8 @@ import {
     VolumeUpOutlined,
 } from '@material-ui/icons';
 
-// Render a YouTube video player
-
 export function CallRecordingPlayer() {
-    const [url, setUrl] = React.useState<string>("https://bigsoundbank.com/UPLOAD/ogg/0267.ogg");
+    const [url] = React.useState<string>("https://bigsoundbank.com/UPLOAD/ogg/0267.ogg");
 
     // Styles
     const useStyles = makeStyles((theme) => ({
@@ -39,14 +35,81 @@ export function CallRecordingPlayer() {
     }));
     const classes = useStyles();
 
+
     // State
     const [audioPlayer, setAudioPlayer] = React.useState<ReactPlayer | null>();
-
+    const [duration, setDuration] = React.useState(0);
+    const [muted, setMuted] = React.useState(false);
     const [played, setPlayed] = React.useState(0);
+    const [playedSeconds, setPlayedSeconds] = React.useState(0);
     const [playing, setPlaying] = React.useState(false);
     const [playbackRate, setPlaybackRate] = React.useState(1.0);
-    const [volume, setVolume] = React.useState(0.8);
+    const [volume] = React.useState(0.8);
 
+
+    // Helper Functions
+    const formatSeconds = (seconds: number) => {
+        // Format as MM:SS
+        return `${Math.floor(seconds / 60).toString().padStart(2, "0")}:${(seconds % 60).toString().padStart(2, "0")}`;
+    };
+
+
+    // Event Handlers
+    const onPlaybackRateSelect_change = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setPlaybackRate(event.target.value as number);
+    };
+
+    const onSkipPreviousButton_click = () => {
+
+    };
+
+    const onReplay10Button_click = () => {
+        audioPlayer?.seekTo(Math.max(playedSeconds - 10, 0), "seconds");
+    };
+
+    const onPlayPauseButton_click = () => {
+        setPlaying(!playing);
+    };
+
+    const onForward10Button_click = () => {
+        audioPlayer?.seekTo(Math.min(playedSeconds + 10, duration), "seconds");
+    };
+
+    const onSkipNextButton_click = () => {
+
+    };
+
+    const onPlaylistButton_click = () => {
+
+    };
+
+    const onVolumeButton_click = () => {
+        setMuted(!muted);
+    };
+
+    const onShareButton_click = () => {
+
+    };
+
+    const onDownloadButton_click = () => {
+
+    };
+
+    const onAudioPlayer_duration = (seconds: number) => {
+        setDuration(Math.round(seconds));
+    };
+
+    const onAudioPlayer_ended = () => {
+        setPlaying(false);
+    };
+
+    const onAudioPlayer_progress = (state: any) => {
+        setPlayed(state.played);
+        setPlayedSeconds(Math.round(state.playedSeconds));
+    };
+
+
+    // Markup
     return <div className={classes.root}>
         <Grid
             container
@@ -62,32 +125,29 @@ export function CallRecordingPlayer() {
                     <Typography>Call Recording - 123-456-7890 - January 1st 12:00 PM</Typography>
                     <Grid container alignItems="center" justify="space-between">
                         <Typography>
-                            00:00
+                            {formatSeconds(playedSeconds)}
                         </Typography>
-                        <Slider min={0} max={0.999999} value={played} />
+                        <Slider style={{ width: 275 }} min={0} max={0.999999} value={played} />
                         <Typography>
-                            00:50
+                            {formatSeconds(duration)}
                         </Typography>
                     </Grid>
                     <Grid container alignItems="center" justify="space-between">
                         <Select
                             value={playbackRate}
-                            onChange={(event: React.ChangeEvent<{ value: unknown }>) => { setPlaybackRate(event.target.value as number); }}
+                            onChange={onPlaybackRateSelect_change}
                         >
                             <MenuItem value={1}>1x</MenuItem>
                             <MenuItem value={1.5}>1.5x</MenuItem>
                             <MenuItem value={2}>2x</MenuItem>
                         </Select>
                         <IconButton>
-                            <SkipPreviousOutlined />
+                            <SkipPreviousOutlined onClick={onSkipPreviousButton_click} />
                         </IconButton>
                         <IconButton>
-                            <Replay10Outlined />
+                            <Replay10Outlined onClick={onReplay10Button_click} />
                         </IconButton>
-                        <IconButton>
-                            <FastRewindOutlined />
-                        </IconButton>
-                        <IconButton onClick={() => { setPlaying(!playing); }}>
+                        <IconButton onClick={onPlayPauseButton_click}>
                             {
                                 playing
                                     ? <PauseCircleOutlineOutlined />
@@ -95,19 +155,39 @@ export function CallRecordingPlayer() {
                             }
                         </IconButton>
                         <IconButton>
-                            <FastForwardOutlined />
+                            <Forward10Outlined onClick={onForward10Button_click} />
                         </IconButton>
                         <IconButton>
-                            <Forward10Outlined />
-                        </IconButton>
-                        <IconButton>
-                            <SkipNextOutlined />
+                            <SkipNextOutlined onClick={onSkipNextButton_click} />
                         </IconButton>
                     </Grid>
                 </Paper>
             </Grid>
             <Grid item>
-                <Paper>More!</Paper>
+                <Paper>
+                    <Grid container alignItems="center" justify="space-between" spacing={2}>
+                        <IconButton onClick={onPlaylistButton_click}>
+                            <PlaylistPlayOutlined />
+                        </IconButton>
+                        <IconButton onClick={onVolumeButton_click}>
+                            {muted
+                                ? <VolumeOffOutlined />
+                                : volume === 0
+                                    ? <VolumeMuteOutlined />
+                                    : volume < 0.5
+                                        ? <VolumeDownOutlined />
+                                        : <VolumeUpOutlined />
+                            }
+                        </IconButton>
+                        <Slider style={{ width: 80 }} min={0} max={0.999999} value={volume} />
+                        <IconButton onClick={onShareButton_click}>
+                            <ShareOutlined />
+                        </IconButton>
+                        <IconButton onClick={onDownloadButton_click}>
+                            <SaveAltOutlined />
+                        </IconButton>
+                    </Grid>
+                </Paper>
             </Grid>
         </Grid>
         <ReactPlayer
@@ -115,15 +195,16 @@ export function CallRecordingPlayer() {
                 setAudioPlayer(player);
             }}
 
+            muted={muted}
             url={url}
             playbackRate={playbackRate}
             played={played}
             playing={playing}
             volume={volume}
 
-            onProgress={(state) => {
-                setPlayed(state.played);
-            }}
+            onDuration={onAudioPlayer_duration}
+            onEnded={onAudioPlayer_ended}
+            onProgress={onAudioPlayer_progress}
 
             config={{
                 file: {
@@ -132,14 +213,4 @@ export function CallRecordingPlayer() {
             }}
         />
     </div>
-
-    // return <Paper>
-    //     <Button onClick={onButtonClick}>Play Recording</Button>
-
-    //     <Grid>
-
-    //     </Grid>
-
-    //     
-    // </Paper>
 }
